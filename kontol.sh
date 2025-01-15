5f2c4f24f794e958c1820d3ade61fa90661af444
gunturyogatama404 <99540625+gunturyogatama404@users.noreply.github.com>
2025-01-15 17:12:54 +0700

    Create kontol.sh

---
+++ kontol.sh
@@ -0,0 +1,2 @@
+wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_131.0.2903.112-1_amd64.deb && \
+apt install ./microsoft-edge-stable_131.0.2903.112-1_amd64.deb -y
