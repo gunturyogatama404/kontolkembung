2cb65eb43cf236d6efc4ecf221ffab9b142be827
gunturyogatama404 <99540625+gunturyogatama404@users.noreply.github.com>
2025-01-15 20:08:01 +0700

    Update kontol.sh

--- kontol.sh
+++ kontol.sh
@@ -1,2 +1,6 @@
 wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_131.0.2903.112-1_amd64.deb && \
 apt install ./microsoft-edge-stable_131.0.2903.112-1_amd64.deb -y
+
+
+
+data-ciphers AES-256-GCM:AES-128-GCM:CHACHA20-POLY1305:AES-128-CBC
